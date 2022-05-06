93edc3fcd957d6e443406e52b4bd84ff2c93baa6
Jeremy Magland <jmagland@flatironinstitute.org>
2022-05-06 13:55:55 -0400

    update example

--- README.md
+++ README.md
@@ -33,15 +33,18 @@ import figurl as fig
 import altair as alt
 from vega_datasets import data
 
+# Create an Altair chart
+# This one comes from the Altair Example Gallery
 stocks = data.stocks()
 
-x = alt.Chart(stocks).mark_line().encode(
+chart = alt.Chart(stocks).mark_line().encode(
   x='date:T',
   y='price',
   color='symbol'
 ).interactive(bind_y=False)
 
-url = fig.Altair(x).url(label='stocks chart')
+# Create and print the figURL
+url = fig.Altair(chart).url(label='stocks chart')
 print(url)
 
 # Output: 
--- examples/altair_vegalite.py
+++ examples/altair_vegalite.py
@@ -1,17 +1,21 @@
-# pip install altair vega_datasets
-
 import figurl as fig
 
 import altair as alt
 from vega_datasets import data
 
+# Create an Altair chart
+# This one comes from the Altair Example Gallery
 stocks = data.stocks()
 
-x = alt.Chart(stocks).mark_line().encode(
+chart = alt.Chart(stocks).mark_line().encode(
   x='date:T',
   y='price',
   color='symbol'
 ).interactive(bind_y=False)
 
-url = fig.Altair(x).url(label='stocks chart')
+# Create and print the figURL
+url = fig.Altair(chart).url(label='stocks chart')
 print(url)
+
+# Output: 
+# https://figurl.org/f?v=gs://figurl/vegalite-2&d=ipfs://bafkreibwifbjrcvxucu3o3373tz74jjkkee3u2t5wrbywzvcoc6q7lxs2i&label=stocks%20chart
